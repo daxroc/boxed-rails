7669610434b6ad311c174f1cfd4ebcd542335559
Damien Roche <droche@gmail.com>
2015-11-15 14:37:04 +0530

    Updated to centos 7.1

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,10 @@ maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures boxed-rails'
 long_description 'Installs/Configures boxed-rails'
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'yum-epel'
 depends 'build-essential'
-
 depends 'poise-ruby-build'
 depends 'application_ruby'
 depends 'application_git'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,8 +6,8 @@
 
 deploy = '/srv/boxed-app'
 app_repo = 'https://github.com/daxroc/boxed-app.git'
-owner = 'webapps'
-group = 'webapps'
+#owner = 'webapps'
+#group = 'webapps'
 
 include_recipe 'yum-epel'
 include_recipe 'boxed-rails::users'
@@ -15,21 +15,21 @@ include_recipe 'boxed-rails::build_essential'
 
 # Create directories
 directory deploy do
-  owner owner
-  group group
+#  owner owner
+#  group group
+#  mode 0754
   action :create
   recursive true
 end
 
 package %w{nodejs git sqlite-devel}
 
-
-#poise_service_user 'webapps'
+poise_service_user 'webapps'
 
 application deploy do
   
-  owner 'webapps'
-  group 'webapps'
+#  owner 'webapps'
+#  group 'webapps'
 
   git app_repo do
     revision 'deploy'
@@ -53,95 +53,4 @@ application deploy do
   end
 
 end
-
-
-# 
-return 
-
-# Create directories
-directory deploy do
-  owner owner
-  group group
-  action :create
-  recursive true
-end
-
-
-# Enable EPEL 
-include_recipe 'yum-epel'
-
-
-# Install required packages
-package 'nodejs'
-package 'git'
-
-
-# Support for Database
-package 'sqlite-devel'
-
-
-git deploy do
-  repository app_repo
-  revision 'deploy'
-  action :sync
-  user owner
-  group group
-end
-
-#node.default['poise-ruby']['install_chef_ruby'] = false
-
-#ruby_runtime '2.2'
-#ruby_gem 'rake'
-#ruby_gem 'bundler'
-
-bundle_install deploy do
-  without %w{development test}
-  deployment true
-end
-
-application deploy do
-  rails do
-    secret_token 'abc123'
-  end
-  unicorn do
-    port 8000
-  end
-end
-
-
-
-
-
-
-return
-
-application '/boxed' do
-  ruby '2.2'
-  ruby_gem 'bundler'
-  owner owner
-  group group
-end
-
-application '/boxed' do
-  owner owner
-  group group
-#  ruby '2.2'
-#  ruby_gem 'bundler'
-  git do
-    repository app_repo
-    checkout_branch 'deploy'
-  end
-  rails do
-#    database 'sqlite3:///db.sqlite3'
-    secret_token 'dbajaosjasof30'
-#    migrate true
-  end
-  bundle_install do
-    deployment true
-    without %w{development test}
-  end
-  unicorn do
-    port 8000
-  end
-end 
 
